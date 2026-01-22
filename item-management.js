ab3be95527f10d60d9f08d75bf464849df968f8e
1prcntethan <ethakari777@gmail.com>
2026-01-21 21:12:41 -0800

    more work on search and card n stuff

--- item-management.js
+++ item-management.js
@@ -1,42 +1,68 @@
-import { collection, getDocs } 
-  from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import {
+  collection,
+  getDocs,
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 import { db } from "/firebase.js";
 
-const querySnapshot = await getDocs(collection(db, "itemId"));
-
-querySnapshot.forEach((doc) => {
-    console.log(doc.id, doc.data());
-});
+let allItems = [];
+const container = document.getElementById("items-container");
+const searchInput = document.getElementById("search");
+const itemCount = document.getElementById("item-count");
 
 async function loadItems() {
   const itemsRef = collection(db, "itemId");
   const snapshot = await getDocs(itemsRef);
 
+  allItems = [];
+
   snapshot.forEach((doc) => {
-    const data = doc.data();
+    allItems.push({
+      id: doc.id,
+      ...doc.data(),
+    });
+  });
 
-    console.log("ITEM ID:", doc.id);
-    console.log("ITEM DATA:", data);
+  renderItems(allItems);
+  itemCount.textContent = `${allItems.length} items found • Help reunite lost belongings with their
+  owners!!`;
+}
 
-    renderItem(data);
+function renderItems(items) {
+  container.innerHTML = "";
+
+  items.forEach((item) => {
+    const div = document.createElement("div");
+    div.className = "bg-white p-4 rounded shadow";
+
+    div.innerHTML = `
+    <p class="text-lg font-bold text-black">${item.name}</p>
+    <p class="text-gray-600">${item.description}</p>
+    <p class="text-sm text-gray-500">Found: ${item.dateFound.toDate().toLocaleDateString('en-US')}</p>
+    <p class="text-sm text-black">Tags: ${item.tags?.join(", ")}</p>
+  `;
+
+    container.appendChild(div);
   });
 }
 
+searchInput.addEventListener("input", () => {
+  const query = searchInput.value.toLowerCase();
 
-function renderItem(item) {
-  const container = document.getElementById("items-container");
+  const filtered = allItems.filter((item) => {
+    
+    const textMatch = (
+      item.name.toLowerCase().includes(query) ||
+      item.description.toLowerCase().includes(query)
+    );
 
-  const div = document.createElement("div");
-  div.className = "bg-white p-4 rounded shadow";
 
-  div.innerHTML = `
-    <p class="text-lg font-bold text-black">${item.name}</p>
-    <p class="text-gray-600">${item.description}</p>
-    <p class="text-sm text-gray-500">Found: ${item.dateFound}</p>
-    <p class="text-sm text-black">Tags: ${item.tag?.join(", ")}</p>
-  `;
+    return textMatch;
+  });
 
-  container.appendChild(div);
-}
+
+  itemCount.textContent = `${filtered.length} items found • Help reunite lost belongings with their
+  owners!!`;
+  renderItems(filtered);
+});
 
 loadItems();
